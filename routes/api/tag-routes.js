96aaa65dee12d02d35a6fb3f7f6d85b8615f61d4
ikaera <ikaera@gmail.com>
2023-03-11 16:09:22 -0500

    update tag routes.

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -21,7 +21,7 @@ router.get("/:id", async (req, res) => {
   // be sure to include its associated Product data
   try {
     const tagData = await Tag.findByPk(req.params.id, {
-      include: [{ model: Product, through: ProductTag, as: "tagged_product" }],
+      include: [{ model: Product }],
     });
 
     if (!tagData) {
